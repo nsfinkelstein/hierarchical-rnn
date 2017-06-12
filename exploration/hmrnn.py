296dc9e85e89e4d99ad2c32ee16713152e2d5259
noam finkelstein <noam.finkelstein@gmail.com>
2017-06-12 18:21:39 -0400

    WIP: Fix step function

--- exploration/hmrnn.py
+++ exploration/hmrnn.py
@@ -1,5 +1,8 @@
 import tensorflow as tf
 import numpy as np
+import scipy.stats as st
+
+# TODO: allow for different size input and hidden state
 
 
 class hmlstm(object):
@@ -13,31 +16,7 @@ class hmlstm(object):
         self.z_below = tf.placeholder(tf.float32, shape=(1))
         self.layers = [self.hmlstm_layer() for _ in range(layers)]
 
-    def run():
-        init = tf.initialize_all_variables()
-        session = tf.Session()
-        session.run(init)
-
-    def get_operation(self, z, z_below):
-        # TODO: Make sure this is actually checking for equality
-        if z == 1:
-            return 'flush'
-
-        if z == 0 and z_below == 1:
-            return 'update'
-
-        if z == 0 and z_below == 0:
-            return 'copy'
-
-        raise ValueError('Invalid indicators')
-
     def hmlstm_layer(self):
-        # figure out operation
-        operation = self.get_operation(self.z, self.z_below)
-
-        if operation == 'copy':
-            return self.c, self.h, self.z
-
         # set biases and weights
         weight_size = (self.state_size * 4) + 1
         b = tf.Variable(np.zeros((weight_size, 1)), dtype=tf.float32)
@@ -60,43 +39,75 @@ class hmlstm(object):
         i = tf.sigmoid(joint_input[self.state_size:2 * self.state_size])
         o = tf.sigmoid(joint_input[2 * self.state_size:3 * self.state_size])
         g = tf.tanh(joint_input[3 * self.state_size:4 * self.state_size])
-        z_tilde = tf.contrib.keras.backend.hard_sigmoid(joint_input[-1])
+        z_tilde = tf.sigmoid(joint_input[-1:])
 
-        new_z = tf.constant(np.random.binomial(1, z_tilde))
+        # TODO: FIX CONDITIONAL
+        print(z_tilde.get_shape())
+        new_z = tf.case(
+            {
+                tf.greater(z_tilde[0], tf.constant(.5, dtype=tf.float32)[0]):
+                lambda: np.ones(1),
+                tf.less_equal(z_tilde[0], tf.constant(.5, dtype=tf.float32)[0]):
+                lambda: np.zeros(1),
+            }, default=lambda: np.zeros(1)
+        )
 
-        if operation == 'update':
-            new_c = tf.add(tf.multiply(f, self.c), tf.multiply(i, g))
+        # copy is handled in the run method
+        def update():
+            return tf.add(tf.multiply(f, self.c), tf.multiply(i, g))
 
-        if operation == 'flush':
-            new_c = tf.multiply(i, g)
+        def flush():
+            return tf.multiply(i, g)
+
+        # TODO: Make sure this condition actually works
+        new_c = tf.cond(tf.equal(self.z, tf.placeholder(1)), flush, update)
 
         new_h = tf.multiply(o, tf.tanh(new_c))
         return new_c, new_h, new_z
 
-    def hmlstm_module(self):
-        for l in self.layers:
-            placeholders = {
-                self.c: 1,
-                self.h: 1,
-                self.below: 1,
-                self.h_above: 1,
-                self.z: 1,
-                self.z_below: 1,
-            }
-            session.run(
-                [], )
-        lowest_c, lowest_h, lowest_z = self.hmlstm_layer(tf.constant())
+    def run(self, signal, epochs=100):
+        init = tf.global_variables_initializer()
+        session = tf.Session()
+        session.run(init)
+
+        # for first run
+        ones = np.ones((self.state_size, 1))
+        last_run = [(ones, ones, np.ones(1)) for _ in self.layers]
+        for _ in range(epochs):
+
+            for t, s in enumerate(signal):
+                current_run = []
+                print(t)
+
+                for i, l in enumerate(self.layers):
+                    # short circut copy operator
+                    if last_run[i][2] == 0 and current_run[i - 1][2] == 0:
+                        current_run[i] = last_run[i]
+                        continue
+
+                    if i == len(self.layers) - 1:
+                        ha = np.zeros((self.state_size, 1))
+                    else:
+                        ha = last_run[i + 1][1]
+
+                    placeholders = {
+                        self.c:
+                        last_run[i][0],
+                        self.h:
+                        last_run[i][1],
+                        self.z:
+                        last_run[i][2],
+                        self.h_below:
+                        s.reshape(-1, 1) if i == 0 else last_run[i - 1][1],
+                        self.h_above:
+                        ha,
+                        self.z_below:
+                        np.ones(1) if i == 0 else last_run[i - 1][2],
+                    }
+
+                    current_run[i] = session.run(l, placeholders)
+
+                last_run = current_run
 
     def output_module(inputs):
         pass
-
-
-x = np.linspace(0, 300, 10000)
-signal = np.sin(x * 3) + np.sin(x * .5) * 5 + np.sin(x * .1) * -2
-
-init = tf.initialize_all_variables()
-session = tf.Session()
-session.run(init)
-
-for t in range(len(signal)):
-    lowest_c, lowest_h, lowest_z = hmlstm_layer()

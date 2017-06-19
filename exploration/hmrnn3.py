2ceb200de5ad6f0c368887317274738b20a05bcf
noam finkelstein <noam.finkelstein@gmail.com>
2017-06-19 14:08:07 -0400

    Improve integration with tensorflow classes

--- exploration/hmrnn3.py
+++ exploration/hmrnn3.py
@@ -9,8 +9,10 @@ HMLSTMState = collections.namedtuple('HMLSTMCellState', ('c', 'h', 'z'))
 
 
 class HMLSTMCell(rnn_cell_impl.RNNCell):
-    def __init__(self, num_units):
+    def __init__(self, num_units, batch_size):
+        super(HMLSTMCell, self).__init__(_reuse=None)
         self._num_units = num_units
+        self._batch_size = batch_size
 
     @property
     def state_size(self):
@@ -24,80 +26,101 @@ class HMLSTMCell(rnn_cell_impl.RNNCell):
 
     def zero_state(self, batch_size, dtype):
         return HMLSTMState(
-            c=tf.zeros([self._num_units]),
-            h=tf.zeros([self._num_units]),
-            z=tf.zeros(()))
+            c=tf.zeros([batch_size, self._num_units]),
+            h=tf.zeros([batch_size, self._num_units]),
+            z=tf.zeros([batch_size]))
 
-    def __call__(self, inputs, state):
+    def call(self, inputs, state):
         """Hierarchical multi-scale long short-term memory cell (HMLSTM)"""
         c, h, z = state
 
         in_splits = tf.constant(([self._num_units] * 2) + [1])
-        print(inputs)
         ha, hb, zb = array_ops.split(
             value=inputs, num_or_size_splits=in_splits, axis=2)
 
         s_recurrent = h
-        s_above = tf.multiply(z, ha)
-        s_below = tf.multiply(zb, hb)
+        expanded_z = tf.expand_dims(tf.expand_dims(z, -1), -1)
+        s_above = tf.squeeze(tf.multiply(expanded_z, ha))
+        s_below = tf.squeeze(tf.multiply(zb, hb))
 
         length = 4 * self._num_units + 1
         states = [s_recurrent, s_above, s_below]
-        concat = rnn_cell_impl._linear(states, length, bias=True)
+        concat = rnn_cell_impl._linear(states, length, bias=True,
+                                       bias_initializer=tf.random_normal_initializer,
+                                       kernel_initializer=tf.random_normal_initializer)
 
         gate_splits = tf.constant(
-            ([self._num_inputs] * 4) + [1], dtype=tf.int32)
+            ([self._num_units] * 4) + [1], dtype=tf.int32)
 
         i, g, f, o, z_tilde = array_ops.split(
             value=concat, num_or_size_splits=gate_splits, axis=1)
 
         new_c = self.calculate_new_cell_state(c, g, i, f, z, zb)
-        new_h = self.calculate_new_cell_state(h, o, new_c, z, zb)
+        new_h = self.calculate_new_hidden_state(h, o, new_c, z, zb)
         new_z = self.calculate_new_indicator(z_tilde)
 
-        output = array_ops.concat((new_h, new_z), axis=1)
+        output = array_ops.concat((new_h, tf.expand_dims(new_z, -1)), axis=1)
         new_state = HMLSTMState(new_c, new_h, new_z)
         return output, new_state
 
     def calculate_new_cell_state(self, c, g, i, f, z, zb):
         # update c and h according to correct operations
-        def copy_c():
-            return c
+        # must do each batch independently
+        new_c = [0] * self._batch_size
+        for b in range(self._batch_size):
 
-        def update_c():
-            return tf.add(tf.multiply(f, c), tf.multiply(i, g))
+            def copy_c():
+                return c[b]
 
-        def flush_c():
-            return tf.multiply(i, g, name='c')
+            def update_c():
+                return tf.add(tf.multiply(f[b], c[b]), tf.multiply(i[b], g[b]))
 
-        new_c = tf.case(
-            [
-                (tf.equal(z, tf.constant(1., dtype=tf.float32)), flush_c),
-                (tf.logical_and(
-                    tf.equal(z, tf.constant(0., dtype=tf.float32)),
-                    tf.equal(zb, tf.constant(0., dtype=tf.float32))), copy_c),
-                (tf.logical_and(
-                    tf.equal(z, tf.constant(0., dtype=tf.float32)),
-                    tf.equal(zb, tf.constant(1., dtype=tf.float32))),
-                 update_c),
-            ],
-            default=update_c,
-            exclusive=True)
-        return new_c
+            def flush_c():
+                return tf.multiply(i[b], g[b], name='c')
+
+            new_c[b] = tf.case(
+                [
+                    (tf.equal(
+                        tf.squeeze(z[b]), tf.constant(1., dtype=tf.float32)),
+                     flush_c),
+                    (tf.logical_and(
+                        tf.equal(
+                            tf.squeeze(z[b]), tf.constant(
+                                0., dtype=tf.float32)),
+                        tf.equal(
+                            tf.squeeze(zb[b]),
+                            tf.constant(0., dtype=tf.float32))), copy_c),
+                    (tf.logical_and(
+                        tf.equal(
+                            tf.squeeze(z[b]), tf.constant(
+                                0., dtype=tf.float32)),
+                        tf.equal(
+                            tf.squeeze(zb[b]),
+                            tf.constant(1., dtype=tf.float32))), update_c),
+                ],
+                default=update_c,
+                exclusive=True)
+
+        return tf.stack(new_c, axis=0)
 
     def calculate_new_hidden_state(self, h, o, new_c, z, zb):
-        def copy_h():
-            return h
+        new_h = [0] * self._batch_size
+        for b in range(self._batch_size):
 
-        def update_h():
-            return tf.multiply(o, tf.tanh(new_c))
+            def copy_h():
+                return h[b]
 
-        new_h = tf.cond(
-            tf.logical_and(
-                tf.equal(z, tf.constant(0., dtype=tf.float32)),
-                tf.equal(zb, tf.constant(0., dtype=tf.float32))), copy_h,
-            update_h)
-        return new_h
+            def update_h():
+                return tf.multiply(o[b], tf.tanh(new_c[b]))
+
+            new_h[b] = tf.cond(
+                tf.logical_and(
+                    tf.equal(
+                        tf.squeeze(z[b]), tf.constant(0., dtype=tf.float32)),
+                    tf.equal(
+                        tf.squeeze(zb[b]), tf.constant(0., dtype=tf.float32))),
+                copy_h, update_h)
+        return tf.stack(new_h, axis=0)
 
     def calculate_new_indicator(self, z_tilde):
         # use slope annealing trick
@@ -117,6 +140,7 @@ class MultiHMLSTMCell(rnn_cell_impl.RNNCell):
     """HMLSTM cell composed squentially of individual HMLSTM cells."""
 
     def __init__(self, cells):
+        super(MultiHMLSTMCell, self).__init__(_reuse=None)
         self._cells = cells
 
     def zero_state(self, batch_size, dtype):
@@ -133,7 +157,7 @@ class MultiHMLSTMCell(rnn_cell_impl.RNNCell):
     def output_size(self):
         return self._cells[-1].output_size
 
-    def __call__(self, inputs, state):
+    def call(self, inputs, state):
         """Run this multi-layer cell on inputs, starting from state."""
         raw_inp = inputs[:, :, :-sum(c._num_units for c in self._cells)]
 
@@ -144,19 +168,20 @@ class MultiHMLSTMCell(rnn_cell_impl.RNNCell):
 
         z_below = tf.ones([tf.shape(inputs)[0], 1, 1])
 
+        raw_inp = array_ops.concat([raw_inp, z_below], axis=2)
+
         new_states = []
         for i, cell in enumerate(self._cells):
             with vs.variable_scope("cell_%d" % i):
                 cur_state = state[i]
 
-                cur_inp = array_ops.concat([raw_inp, h_aboves[i], z_below], axis=2)
+                print(i)
+                cur_inp = array_ops.concat([raw_inp, h_aboves[i]], axis=2)
                 raw_inp, new_state = cell(cur_inp, cur_state)
+                raw_inp = tf.expand_dims(raw_inp, 1)
                 new_states.append(new_state)
 
-            new_states = tuple(new_states)
-
-        hidden_states = [ns['h'] for ns in new_states]
-
+        hidden_states = [ns[1] for ns in new_states]
         return hidden_states, tuple(new_states)
 
 
@@ -168,25 +193,28 @@ class MultiHMLSTMNetwork(object):
         self._num_units = num_units  # the length of c and h
 
         batch_shape = (batch_size, truncate_len, num_units)
-        self.batch_in = tf.placeholder(tf.float32, shape=batch_shape,
-                                       name='batch_in')
+        self.batch_in = tf.placeholder(
+            tf.float32, shape=batch_shape, name='batch_in')
 
-        self.batch_out = tf.placeholder(tf.float32, shape=batch_shape,
-                                        name='batch_out')
+        self.batch_out = tf.placeholder(
+            tf.int32, shape=[batch_size, truncate_len], name='batch_out')
 
         self.train, self.loss = self.create_network(self.create_output_module)
 
     def gate_input(self, hidden_states):
         # gate the incoming hidden states
-        with vs.variable_scope('gates'):
+        with vs.variable_scope('gates', reuse=True):
             gates = []
             for l in range(self._num_layers):
                 weights = vs.get_variable(
-                    'gate_%s' % l, [self._num_units * 4, 1], dtype=tf.float32)
-                gates.append(tf.sigmoid(tf.matmul(weights, hidden_states)))
+                    'gate_%s' % l, [self._num_units * self._num_layers, 1],
+                    dtype=tf.float32)
+                gates.append(tf.sigmoid(tf.matmul(hidden_states, weights)))
 
             split = array_ops.split(
-                value=hidden_states, num_or_size_splits=self._num_layers)
+                value=hidden_states,
+                num_or_size_splits=self._num_layers,
+                axis=1)
             gated_list = []
             for gate, hidden_state in zip(gates, split):
                 gated_list.append(tf.multiply(gate, hidden_state))
@@ -194,7 +222,7 @@ class MultiHMLSTMNetwork(object):
             gated_input = tf.concat(gated_list, axis=1)
         return gated_input
 
-    def create_output_module(self, gated_input):
+    def create_output_module(self, gated_input, time_step):
         embed_size = 100
         out_hidden_size = 100
         with vs.variable_scope('output_module'):
@@ -204,9 +232,9 @@ class MultiHMLSTMNetwork(object):
             embed_weights = vs.get_variable(
                 'embed_weights', embed_shape, dtype=tf.float32)
 
-            b1 = vs.get_variable('b1', [out_hidden_size, 1], dtype=tf.float32)
-            b2 = vs.get_variable('b2', [out_hidden_size, 1], dtype=tf.float32)
-            b3 = vs.get_variable('b3', [self._num_units, 1], dtype=tf.float32)
+            b1 = vs.get_variable('b1', [1, out_hidden_size], dtype=tf.float32)
+            b2 = vs.get_variable('b2', [1, out_hidden_size], dtype=tf.float32)
+            b3 = vs.get_variable('b3', [1, self._num_units], dtype=tf.float32)
             w1 = vs.get_variable(
                 'w1', [embed_size, out_hidden_size], dtype=tf.float32)
             w2 = vs.get_variable(
@@ -216,37 +244,48 @@ class MultiHMLSTMNetwork(object):
 
             # embedding
             prod = tf.matmul(gated_input, embed_weights)
-            embedding = tf.nn.relu(tf.reduce_sum(prod, axis=1))
+            embedding = tf.nn.relu(prod)
 
             # feed forward network
             # first layer
-            l1 = tf.nn.tanh(tf.matmul(w1, embedding) + b1)
+            l1 = tf.nn.tanh(tf.matmul(embedding, w1) + b1)
 
             # second layer
-            l2 = tf.nn.tanh(tf.matmul(w2, l1) + b2)
+            l2 = tf.nn.tanh(tf.matmul(l1, w2) + b2)
 
             # the loss function used below
             # sparse_softmax_cross_entropy_with_logits
-            prediction = tf.matmul(w3, l2) + b3
+            prediction = tf.matmul(l2, w3) + b3
 
             loss = tf.nn.sparse_softmax_cross_entropy_with_logits(
-                labels=self.batch_out, logits=tf.stack(prediction))
+                labels=self.batch_out[:, time_step], logits=prediction)
         return loss
 
     def create_network(self, output_module):
-        hmlstm_cell = HMLSTMCell(self._num_units)
-        hmlstm = MultiHMLSTMCell([hmlstm_cell] * self._num_layers)
+        def hmlstm_cell():
+            return HMLSTMCell(self._num_units, self._batch_size)
+        hmlstm = MultiHMLSTMCell([hmlstm_cell() for _ in range(self._num_layers)])
 
         state = hmlstm.zero_state(self._batch_size, tf.float32)
-        h_aboves = tf.zeros([self._batch_size, 1, (self._num_layers * self._num_units)])
+        h_aboves = tf.zeros(
+            [self._batch_size, 1, (self._num_layers * self._num_units)])
         loss = 0.0
         for i in range(self._truncate_len):
-            inputs = array_ops.concat((self.batch_in[:, i:(i+1)], h_aboves), axis=2)
+            inputs = array_ops.concat(
+                (self.batch_in[:, i:(i + 1)], h_aboves), axis=2)
 
             hidden_states, state = hmlstm(inputs, state)
-            concated_hs = array_ops.concat(hidden_states[1:], axis=0)
-            h_aboves[:, 0, :-self._num_units] = concated_hs
-            loss += output_module(concated_hs)
+            concated_hs = array_ops.concat(hidden_states[1:], axis=1)
+            h_aboves = array_ops.concat(
+                [
+                    concated_hs, tf.zeros(
+                        [self._batch_size, self._num_units], dtype=tf.float32)
+                ],
+                axis=1)
+            h_aboves = tf.expand_dims(h_aboves, 1)
+
+            loss += output_module(
+                self.gate_input(array_ops.concat(hidden_states, axis=1)), i)
 
         train = tf.train.AdamOptimizer(1e-4).minimize(loss)
         return train, loss
@@ -268,6 +307,7 @@ import numpy as np
 batch_size = 10
 truncate_len = 100
 
+
 def text():
     text = load_text()
     text = text.replace('\n', ' ')
@@ -275,6 +315,7 @@ def text():
 
     return [(one_hot_encode(intext), one_hot_encode(outtext))
             for intext, outtext in signals]
+
 
 def load_text():
     with open('text.txt', 'r') as f:
@@ -287,6 +328,7 @@ def load_text():
         signals.append((intext, outtext))
 
     return signals
+
 
 def one_hot_encode(text):
     out = np.zeros((len(text), 29))
@@ -307,6 +349,7 @@ def one_hot_encode(text):
 
     return out
 
+
 def run_everything():
     network = MultiHMLSTMNetwork(batch_size, 3, truncate_len, 29)
     y = text()
@@ -315,9 +358,10 @@ def run_everything():
     for batch_number in range(10):
         start = batch_number * batch_size
         end = (1 + batch_number) * batch_size
-        batches_in.append([i for i, _ in y[start: end]])
+        batches_in.append([i for i, _ in y[start:end]])
         batches_out.append([o for _, o in y[start:end]])
     network.run(batches_in, batches_out)
+
 
 if __name__ == '__main__':
     run_everything()

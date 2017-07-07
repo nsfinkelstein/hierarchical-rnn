648ba7fc2f036c1d591489002a18404a8e8246bd
noam finkelstein <noam.finkelstein@gmail.com>
2017-07-07 15:42:56 -0400

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='hmlstm',
+    version='0.0.1',
+    description='Implementation of hierarchical multiscale lstm network',
+    url='https://github.com/n-s-f/hierarchical-rnn',
+    packages=find_packages(exclude=['contrib', 'docs', 'tests', 'examples']),
+)

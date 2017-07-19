fix loss calculation when task is regression

hmlstm/hmlstm_network.py
@@ -264,8 +264,9 @@ class HMLSTMNetwork(object):
 
         mapped = tf.map_fn(map_output, to_map)
 
-        loss = tf.reduce_mean(mapped[:, :, 0])
-        predictions = mapped[:, :, 1:]
+        # loss has diffenent shape for task 'regression' and 'loss'
+        loss = tf.reduce_mean(mapped[:, :, :-self._output_size])
+        predictions = mapped[:, :, -self.output_size:]
         train = self._optimizer.minimize(loss)
 
         return train, loss, indicators, predictions
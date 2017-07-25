e12d0d461373cdcc73f78ab21e024ed66220d4cc
igorkulev <igor.kulev@gmail.com>
2017-07-25 16:08:25 +0200

    Update viz.py
    Fixed bug in division

--- hmlstm/viz.py
+++ hmlstm/viz.py
@@ -11,7 +11,7 @@ def plot_indicators(truth, prediction, indicators):
     for l, layer in enumerate(indicators):
         for i, indicator in enumerate(layer):
             if indicator == 1.:
-                p = 1 / len(indicators)
+                p = 1.0 / len(indicators)
                 ymin = p * l
                 ymax = p * (l + 1)
                 ax.axvline(i, color=colors[l], ymin=ymin, ymax=ymax, alpha=.3)
